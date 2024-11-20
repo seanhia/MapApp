40e463f49d7656f6b7f78af37ecc0bd4eae1b4ea
Sean <seanhia15@gmail.com>
2024-11-19 16:58:20 -0800

    Basic login page setup + added GoogleServiceInfo.plist

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import { useState} from "react";
+import { Text, View, StyleSheet, KeyboardAvoidingView, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function Index() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const signUp = () => {
+
+  }
+  const signIn = () => {
+    
+  }
+
+
+
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>EXPLORE APP made by CLOUD</Text>
+      <KeyboardAvoidingView behavior="padding">
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder = "Email"
+            />
+            <TextInput
+              style={styles.input}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              placeholder = "Password"
+
+            />
+            <Button onPress={signUp} title="Sign Up" />
+            <Button onPress={signIn} title="Sign In" />
+
+
+        </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
