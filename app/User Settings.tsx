f6f2efe3df23e3cdc9e2f9737fd9d10afd7615a8
Jade <Jade.MartinezPena01@student.csulb.edu>
2024-12-04 16:02:20 -0800

    UI: user settings

--- app/User Settings.tsx
+++ app/User Settings.tsx
@@ -1,16 +1,85 @@
 import SettingsHeader from '@/components/SettingsHeader';
-import React from 'react';
-import { View, Text,Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch, Image } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 const UserSettings = () => {
 
-    
-    return(
-        <View style={{flex: 1, backgroundColor: 'white'}}>
-            <SettingsHeader/>
-            <TextInput style={{height: 50, margin:12, borderWidth:1, padding: 10 }}>
-            </TextInput>
+    const [is_enable, setIsEnable] = useState(false);
+    const toggleSwitch = () => setIsEnable(previousState => !previousState);
+
+    const [display_name, onChangeDisplayName] = useState('');
+    const [username, onChangeUsername] = useState('');
+    const [bio, onChangeBio] = useState('');
+    const [email, onChangeEmail] = useState('')
+    const [password, onChangePassword] = useState('')
+    const [phone_number, onChangePhoneNumber] = useState('')
+
+
+
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <SettingsHeader />
+            <View style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Text>Private account</Text>
+                    <Switch
+                        trackColor={{ false: '#767577', true: '#FFE699' }}
+                        thumbColor={is_enable ? '#f5dd4b' : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={toggleSwitch}
+                        value={is_enable}
+                    />
+                </View>
+
+            </View>
+
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangeDisplayName}
+                value={display_name}
+                placeholder="Display name"
+            />
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangeUsername}
+                value={username}
+                placeholder="Username"
+            />
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangeBio}
+                value={bio}
+                placeholder="Bio"
+            />
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangeEmail}
+                value={email}
+                placeholder="Email"
+            />
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangePassword}
+                value={password}
+                placeholder="Password"
+            />
+            <TextInput
+                style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}
+                onChangeText={onChangePhoneNumber}
+                value={phone_number}
+                placeholder="Phone number"
+            />
+            <View style={{ height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Text>Tutorial</Text>
+                    <Text style={{ fontSize: 24 }}>></Text>
+                </View>
+            </View>
+
+
+
+
         </View>
     );
 };
--- components/SettingsHeader.tsx
+++ components/SettingsHeader.tsx
@@ -3,10 +3,12 @@ import { View, Text, Image } from 'react-native';
 
 const SettingsHeader = () => {
     return (
-        <View style={{ paddingHorizontal: 15 }}>
+        <View style={{ paddingHorizontal: 15, }}>
             <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingTop: 15 }}>
-                <Image style={{ height: 100, width: 100 }} source={require('../assets/images/profile-pic.jpg')} />
-                 <Text>Edit profile picture</Text>
+                <Image style={{ height: 125, width: 125 }} source={require('../assets/images/profile-pic.jpg')} />
+                <View style={{height: 50, margin: 12, borderWidth: 1, padding: 10, backgroundColor: '#FBE8C5'}}>
+                    <Text style={{ marginTop: '3.5%' }}>Edit profile picture</Text>
+                </View>
             </View>
         </View>
 
