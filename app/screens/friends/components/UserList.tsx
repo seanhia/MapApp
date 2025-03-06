5b2612c48a34de88466f32b2512ec00066e79844
Sophia Thomas <sophiart03@gmail.com>
2025-03-05 22:24:26 -0800

    Search Bar: No User's Found

--- app/screens/friends/components/UserList.tsx
+++ app/screens/friends/components/UserList.tsx
@@ -13,40 +13,53 @@ interface UserListProps {
 const UserList: React.FC<UserListProps> = ({ users, visible }) => {
   const { colorScheme, styles } = useTheme();
   if (!visible) return null;
-
+  const noUsers = users.length == 0; 
+  
+  if (noUsers) {
+    return (
+      <View style={styles.userListContainer}>
+        <Text style={[styles.text, {color: 'black'}]}>No Users Found...</Text>
+      </View>
+  )}
   return (
     <View style={[styles.userListContainer]}>
       
-    <FlatList
-      data={users}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
 
-        <View style={styles.listContainer}>
-          <View style={styles.leftContainer}>
-          <Image
-              style={{width: 70, height: 70, margin: 10, borderRadius: 50}}
-               source={require('@/assets/images/profile-pic.jpg')}
-            />
-          <Text style={[styles.text, {fontWeight: 'bold'}]}>
-          {item.username}
-          </Text>
+          <View style={styles.listContainer}>
+
+            <View style={styles.leftContainer}>
+              <Image // Profile Picture ({imageURL, dimensions[height, width, borderRadisu]}: Props) => {}
+                  style={{width: 50, height: 50, borderRadius: 30}}
+                  source={require('@/assets/images/profile-pic.jpg')}
+                />
+              <Text style={[styles.boldText, {color: 'black'}]}>
+                {item.username}
+              </Text>
+            </View>
+
+            {/** Button 
+             * 
+             * ToDo: create a button component
+             * Button ({handleClick, text, style}: Props) => {}
+            */}
+            <TouchableOpacity 
+              style={styles.addFriendButton}
+              onPress={() => createFriendship(item)} 
+              >
+                <Text 
+                  style={{fontWeight: 'bold', color: 'black', }}>
+                  Add Friend
+                </Text>
+            </TouchableOpacity>
+        
           </View>
-          <TouchableOpacity
-          style={styles.addFriendButton}
-          onPress={() => createFriendship(item)} // Use dynamic user ID and username
-            >
-          <Text 
-            style={{fontWeight: 'bold', color: 'black', }}>
-            Add Friend
-          </Text>
+        )}
+      />
 
-        </TouchableOpacity>
-      
-      </View>
-      
-      )}
-    />
     </View>
   );
 };
