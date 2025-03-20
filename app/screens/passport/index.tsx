ad9b1a0cd5b102dd35a8eef49a640f2ca4fe86dd
Sean <seanhia15@gmail.com>
2025-03-19 23:13:54 -0700

    virtual passport button on profile page

---
+++ app/screens/passport/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const passport = () => {
+    return (
+        <View>
+            <Text>Virtual Passport Screen</Text>
+        </View>
+    )
+
+}
+
+export default passport;
