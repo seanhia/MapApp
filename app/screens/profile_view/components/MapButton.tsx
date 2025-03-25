89248592a0fa57655a93e8a732be7bd3ed11b38f
Sophia Thomas <sophiart03@gmail.com>
2025-03-25 10:05:26 -0700

    Update MapButton UI

--- app/screens/profile_view/components/MapButton.tsx
+++ app/screens/profile_view/components/MapButton.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from "react";
-import { View, Button, StyleSheet} from "react-native";
+import { View, TouchableOpacity, Text} from "react-native";
 import { User, status, Friend} from "@/data/types";
 import { useRouter} from "expo-router";
 import { fetchCurrentUser } from '@/data/UserDataService';
 import { existingFriendshipQuery } from '@/data/Friendship';
+import { useTheme } from '@/hooks/useTheme'
 
 
 export const MapButton = ({user}: {user: User | null}) => {
     const router = useRouter();
+    const { styles } = useTheme(); 
     const [friendship, setFriendship] = useState<boolean>(true);
     const [loading, setLoading] = useState<boolean>(true); 
 
@@ -39,10 +41,13 @@ export const MapButton = ({user}: {user: User | null}) => {
 
     return (
         <View>
-            <Button
-                title={`View ${user?.username}'s Map`}
+            <TouchableOpacity
+                style={[styles.sideButton, {}]}
+                // title={`View ${user?.username}'s Map`}
                 onPress={()=> router.push({pathname: "/screens/friend_map", params: {friendId: user?.id}})}
-            />
+            >
+              <Text style={styles.buttonText}>{`View ${user?.username}'s Map`}</Text>
+            </TouchableOpacity>
         </View>
     )
 }
