40e616a19618f144cb866d01a7495958f0a84fea
Sean <seanhia15@gmail.com>
2025-04-03 00:30:07 -0700

    achievements column for virtual passport

--- app/screens/passport/index.tsx
+++ app/screens/passport/index.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { getStats } from '@/firestore';
 import { fetchCurrentUser } from '@/data/UserDataService';
 import { useEffect, useState } from 'react';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 const passport = () => {
     const [userId, setUserId] = useState<string | null>(null);
@@ -35,19 +36,57 @@ const passport = () => {
         fetchStats();
     },[userId])
     return (
-        <div>
-          <h1>
-            Virtual Passport
-          </h1>
-          <ul>
-            {CityCountry.map((stat, index) => (
-              <li key={index}>
-                {stat.cities}, {stat.countries}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <SafeAreaProvider>
+          <SafeAreaView style={style.container}>
+              <Text style={style.title}> Virtual Passport</Text>
+              <View style={style.column}>
+              <View>
+              <ul >
+              <Text style={style.title}> Places Visited</Text>
+              {CityCountry.map((stat, index) => (
+                <li style={style.list} key={index}>
+                  {stat.cities}, {stat.countries}
+                </li>
+              ))}
+            </ul>
+              </View>
+              <View>
+              <Text style={style.title}> Achievements</Text>
+              </View>
+            </View>
+          </SafeAreaView>
+        </SafeAreaProvider>
       );
     };
     
 export default passport;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 6,
+    borderWidth: 6,
+    borderColor: '#20232a',
+    borderRadius: 100,
+    backgroundColor: '#48b3ee',
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  list: {
+    marginTop: 12,
+    padding: 12,
+    borderRadius: 8,
+    color: '#FFFFFF',
+    backgroundColor: '#48b3ee',
+  },
+  column: {
+    flexDirection: 'row',
+  }
+  })
