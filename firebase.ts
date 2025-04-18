54380e97df8b6daf4a7a8cfe789085173b872153
Sophia Thomas <sophiart03@gmail.com>
2025-04-17 17:23:43 -0700

    Add .env

--- firebase.ts
+++ firebase.ts
@@ -1,21 +1,22 @@
 // firebase.js
-import { initializeApp} from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
- 
+
+
 //import firebase from 'firebase/app';
 //import 'firebase/auth'; // Import additional services as needed (e.g., 'firebase/firestore')
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHImvQ-8khMcF0NvTbVAo4vJoTcpxSaKY",
-  authDomain: "mapapp-c59c0.firebaseapp.com",
-  projectId: "mapapp-c59c0",
-  storageBucket: "mapapp-c59c0.firebasestorage.app",
-  messagingSenderId: "298912299170",
-  appId: "1:298912299170:ios:8fdf4e8c45d1ca6bf57ffb"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGEING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 
