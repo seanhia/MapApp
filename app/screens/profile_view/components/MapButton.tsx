0d0f1e6dbcdc7b35f8b7d069d5dfbce9c733086a
Jade <Jade.MartinezPena01@student.csulb.edu>
2025-05-11 07:29:02 -0700

    profile view translations

--- app/screens/profile_view/components/MapButton.tsx
+++ app/screens/profile_view/components/MapButton.tsx
@@ -1,53 +1,73 @@
-import React, {useEffect, useState} from "react";
-import { View, TouchableOpacity, Text} from "react-native";
-import { User, status, Friend} from "@/data/types";
-import { useRouter} from "expo-router";
+import React, { useEffect, useState } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import { User, status, Friend } from "@/data/types";
+import { useRouter } from "expo-router";
 import { fetchCurrentUser } from '@/data/UserDataService';
 import { existingFriendshipQuery } from '@/data/Friendship';
 import { useTheme } from '@/hooks/useTheme'
+import { useTranslation } from 'react-i18next';
 
 
-export const MapButton = ({user}: {user: User | null}) => {
-    const router = useRouter();
-    const { styles } = useTheme(); 
-    const [friendship, setFriendship] = useState<boolean>(true);
-    const [loading, setLoading] = useState<boolean>(true); 
 
-    useEffect(() => {
-        const checkFriendship = async () => {
-          if (!user) {
-            setLoading(false); 
-            return; 
+
+export const MapButton = ({ user }: { user: User | null }) => {
+  const router = useRouter();
+  const { styles } = useTheme();
+  const [friendship, setFriendship] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
+  const { t } = useTranslation();
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [language, setSelectedLanguage] = useState<string>('en');
+
+  useEffect(() => {
+    const checkFriendship = async () => {
+      if (!user) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const currentUser = await fetchCurrentUser();
+        if (currentUser) {
+          const friendStatus = await existingFriendshipQuery(currentUser, user);
+          if (friendStatus[1] == status[1]) {
+            setFriendship(true)
+          } else {
+            setFriendship(false)
           }
-  
-          try {
-            const currentUser = await fetchCurrentUser(); 
-            if (currentUser) {
-              const friendStatus = await existingFriendshipQuery(currentUser, user);
-              if (friendStatus[1] == status[1]) {
-                setFriendship(true)
-              } else {
-                setFriendship(false)
-              }
-          }
-          } catch (error) {
-            console.error('Error checking friendship: ', error);
-          } finally {
-            setLoading(false);
-          }
-        }; 
-          checkFriendship(); 
-      }, [user]); 
+        }
+      } catch (error) {
+        console.error('Error checking friendship: ', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    const loadCurrentUser = async () => {
+      try {
+        const user = await fetchCurrentUser();
+        setCurrentUser(user);
 
-    return (
-        <View>
-            <TouchableOpacity
-                style={[styles.sideButton, {}]}
-                // title={`View ${user?.username}'s Map`}
-                onPress={()=> router.push({pathname: "/screens/friend_map", params: {friendId: user?.id}})}
-            >
-              <Text style={styles.buttonText}>{`View ${user?.username}'s Map`}</Text>
-            </TouchableOpacity>
-        </View>
-    )
+        const lang = user?.language || "en";
+        setSelectedLanguage(lang);
+
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    };
+    checkFriendship();
+    loadCurrentUser();
+
+  }, [user]);
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={[styles.sideButton, {}]}
+        // title={`View ${user?.username}'s Map`}
+        onPress={() => router.push({ pathname: "/screens/friend_map", params: { friendId: user?.id } })}
+      >
+        <Text style={styles.buttonText}> {t('viewMap', { username: user?.username || '' })}</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
--- app/screens/profile_view/components/PrivateProfile.tsx
+++ app/screens/profile_view/components/PrivateProfile.tsx
@@ -1,19 +1,46 @@
-import React, {View, Text, Image} from 'react-native'
+import React, { View, Text, Image } from 'react-native'
 import { useTheme } from '@/hooks/useTheme'
-import lock  from '@/assets/images/private.png'
+import lock from '@/assets/images/private.png'
+import { useEffect, useState } from 'react';
+import { fetchCurrentUser } from '@/data/UserDataService';
+import { User } from '@/data/types'
+import { useTranslation } from 'react-i18next';
 
-export const PrivateProfile =  () => {
-    const { styles } = useTheme(); 
-    const privateProfileMsg = "This account is private"
+
+export const PrivateProfile = () => {
+    const { styles } = useTheme();
+    const { t } = useTranslation();
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [language, setSelectedLanguage] = useState<string>('en');
+
+    useEffect(() => {
+        const loadCurrentUser = async () => {
+            try {
+                const user = await fetchCurrentUser();
+                setCurrentUser(user);
+                
+                const lang = user?.language || "en";
+                setSelectedLanguage(lang);
+
+            } catch (error) {
+                console.error('Error fetching user:', error);
+            }
+        };
+        
+        loadCurrentUser();
+    }, []);
+
+    const privateProfileMsg =  t('private');
+
 
     return (
-        <View style={[{flex: 1, justifyContent: 'center', alignItems: 'center'}]}>
-            <Image 
+        <View style={[{ flex: 1, justifyContent: 'center', alignItems: 'center' }]}>
+            <Image
                 source={lock}
                 style={styles.image}
-             />
+            />
             <Text style={styles.text}>{privateProfileMsg}</Text>
 
         </View>
-    ); 
+    );
 }
