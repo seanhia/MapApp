80fac8fa39baa1a8a53f509fc0a8c37d65f3db92
Jade <Jade.MartinezPena01@student.csulb.edu>
2025-05-10 20:01:31 -0700

    notifications translation

--- app/screens/notifications/index.tsx
+++ app/screens/notifications/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { User, Notification } from '@/data/types'
-import { fetchNotifications, updateNotification} from '@/data/Friendship';
+import { fetchNotifications, updateNotification } from '@/data/Friendship';
 import { router } from "expo-router";
+import { fetchCurrentUser } from '@/data/UserDataService';
+import { useTranslation } from 'react-i18next';
 
 
 
@@ -13,17 +15,22 @@ interface NotificationsProps {
 const Notifications: React.FC<NotificationsProps> = ({ user }) => {
 
   const [notifications, setNotifications] = useState<Notification[]>([]);
+  const { t } = useTranslation();
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [language, setSelectedLanguage] = useState<string>('en');
 
-//updates notifications and displays friend's profile 
+
+
+  //updates notifications and displays friend's profile 
   const handleNotifictaion = async (notification: Notification) => {
     //updates notifications to true and deletes it from the database 
-    const update = await updateNotification(user?.id, notification); 
-      //view friend's profile 
-      router.push({
-        pathname: '/screens/profile_view',
-        params: { userId: notification.postUserId || notification.friendRequestUserId },
-      });
-    
+    const update = await updateNotification(user?.id, notification);
+    //view friend's profile 
+    router.push({
+      pathname: '/screens/profile_view',
+      params: { userId: notification.postUserId || notification.friendRequestUserId },
+    });
+
   };
 
   useEffect(() => {
@@ -31,31 +38,47 @@ const Notifications: React.FC<NotificationsProps> = ({ user }) => {
     if (!user || !user.id) {
       console.error("User ID is undefined. Cannot fetch posts.");
       return;
-      
+
     }
     //loads notifications 
+
+    const loadCurrentUser = async () => {
+      try {
+        const user = await fetchCurrentUser();
+        setCurrentUser(user);
+
+        const lang = user?.language || "en";
+        setSelectedLanguage(lang);
+
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    };
+
+
     const loadNotifications = async () => {
       try {
-          const unsubscribe = await fetchNotifications(user.id, (newNotifications) => {
-              setNotifications(newNotifications); 
-          });
-          
-          return () => {
-              unsubscribe();
-          };
-      } catch (error) {
-          console.error("Error fetching posts:", error); 
-      }
-  };
+        const unsubscribe = await fetchNotifications(user.id, (newNotifications) => {
+          setNotifications(newNotifications);
+        });
 
-  loadNotifications(); 
+        return () => {
+          unsubscribe();
+        };
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    loadNotifications();
+    loadCurrentUser();
 
   }, [user]);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Notifications</Text>
+      <Text style={styles.text}>{t('notif')}</Text>
       {notifications.length === 0 ? (
-        <Text style={[styles.text, { fontWeight: 100 }]}>No new notifications</Text>
+        <Text style={[styles.text, { fontWeight: 100 }]}>{t('no_new_notif')}</Text>
       ) : (
         <FlatList
           data={notifications}
